8d8bc9053b7b3df64b13d1b4aad75f62069d0b44
Keenan <keenan.penner@gmail.com>
2023-10-18 13:41:04 +0200

    added branch path in 3D for standard IDLA

--- idla_tree.py
+++ idla_tree.py
@@ -9,14 +9,8 @@ def sum_list(L1,L2):
         S.append(L1[i]+L2[i])
     return S
 
-def diff_list(L1,L2):
-    S=[]
-    for i in range(len(L1)):
-        S.append(L1[i]-L2[i])
-    return S
 
-def movement(L,p): 
-
+def movement(L,p): #for 2D idla
     if p<0.25:##right##
         
         newposition=sum_list(L[0],[1,0])
@@ -40,8 +34,8 @@ def movement(L,p):
 
     return L, newposition
 
-def prev(L,p):
-    if p<0.25:
+def prev(L,p): # For 2D
+    if p<0.25: 
         previous=sum_list(L, [-1,0])
     elif 0.25<=p<0.5:
         previous=sum_list(L, [1,0])
@@ -51,76 +45,7 @@ def prev(L,p):
         previous=sum_list(L, [0,1])
     return previous
 
-def idla(n):
-    L=[[0,0]]
-    aretes=[]
-    count=1
-    while count<n:
-        
-        p=rd.random()
-        move=movement([[0,0]],p)[1]
-        
-        while move in L:
-            p=rd.random()
-            move=movement([move],p)[1]    
-            
-        if move not in L:
-            previous=prev(move,p)
-            L.append(move)
-            aretes.append([previous,move])
-            count+=1
-    return L,aretes
-
-
-## PLOT 2D TREE
-
-'''A=idla(30000)
-for i in range(len(A)):
-    plt.plot([A[i][0][0],A[i][1][0]],[A[i][0][1],A[i][1][1]],linewidth=0.5)
-    plt.axis('square')
-    plt.axis('off')
-plt.show()
-plt.savefig('IDLA_tree_30000.png', dpi=500)'''
-
-
-
-## 2D TREE WITH COLORED BRANCH 
-
-def branch(aretes,point):
-    B=[]
-    rev=reverse(aretes)
-    for i in range(len(rev)):
-        if rev[i][1]==point:
-            B.append(rev[i])
-            point=rev[i][0]
-    return B
-            
-        
-
-def reverse(aretes):
-    L=[]
-    for i in range( len(aretes) - 1, -1, -1) :
-        L.append(aretes[i])
-    return L
-
-def branchplot(n):
-    A=idla(n)
-    point=A[0][-1]
-    aretes=A[1]
-    Branch=branch(aretes,point)
-    for i in range(len(aretes)):
-        plt.plot([aretes[i][0][0],aretes[i][1][0]],[aretes[i][0][1],aretes[i][1][1]],linewidth=0.5,color='blue')
-    for i in range(len(Branch)):
-        plt.plot([Branch[i][0][0],Branch[i][1][0]],[Branch[i][0][1],Branch[i][1][1]],linewidth=0.5,color='red')
-    plt.axis('square')
-    plt.show()
-    plt.savefig('branchtest.png', dpi=500)
-
-
-### 3D TREE 
-
-
-def movement_bis(L,p): #from a given position, gives the new position 
+def movement_bis(L,p): #for 3D idla
 
     if p<1/6:
         
@@ -155,7 +80,7 @@ def movement_bis(L,p): #from a given position, gives the new position
 
     return L, newposition
 
-def previous(L,p):
+def previous(L,p): #for 3D
     if p<1/6:
         previous=sum_list(L, [-1,0,0])
     elif 1/6<=p<1/3:
@@ -169,6 +94,30 @@ def previous(L,p):
     else:
         previous=sum_list(L, [0,0,1]) 
     return previous
+
+
+#IDLA functions in 2D and 3D
+#These functions return the edges of the idla tree, in dimensions 2 and 3
+
+def idla(n):
+    L=[[0,0]]
+    aretes=[]
+    count=1
+    while count<n:
+        
+        p=rd.random()
+        move=movement([[0,0]],p)[1]
+        
+        while move in L:
+            p=rd.random()
+            move=movement([move],p)[1]    
+            
+        if move not in L:
+            previous=prev(move,p)
+            L.append(move)
+            aretes.append([previous,move])
+            count+=1
+    return L,aretes
 
 def idla3(n):
     L=[[0,0,0]]
@@ -188,13 +137,23 @@ def idla3(n):
             L.append(move)
             aretes.append([prev,move])
             count+=1
-    return aretes
-
-## PLOT 3D TREE
+    return L,aretes
 
 
+## Example for 2D tree 
 
-'''A=idla3(1000)
+'''A=idla(30000)
+for i in range(len(A)):
+    plt.plot([A[i][0][0],A[i][1][0]],[A[i][0][1],A[i][1][1]],linewidth=0.5)
+    plt.axis('square')
+    plt.axis('off')
+plt.show()
+plt.savefig('IDLA_tree_30000.png', dpi=500)'''
+
+
+## Example for 3D tree
+
+'''A=idla3(1000)[0]
 
 ax=plt.axes(projection="3d")
 ax.set_box_aspect([1,1,1])
@@ -205,4 +164,54 @@ for i in range(len(A)):
 plt.axis('off')
 plt.show()
 plt.savefig('IDLA_tree_3D_1.png', dpi=500)'''
+
+
+## 2D TREE WITH COLORED BRANCH
+
+def reverse(aretes):
+    L=[]
+    for i in range( len(aretes) - 1, -1, -1) :
+        L.append(aretes[i])
+    return L
+
+
+def branch(aretes,point): #gives all edges from center to point 
+    B=[]
+    rev=reverse(aretes)
+    for i in range(len(rev)):
+        if rev[i][1]==point:
+            B.append(rev[i])
+            point=rev[i][0]
+    return B
+
+
+def branchplot(n):
+    A=idla(n)
+    point=A[0][-1]
+    aretes=A[1]
+    Branch=branch(aretes,point)
+    for i in range(len(aretes)):
+        plt.plot([aretes[i][0][0],aretes[i][1][0]],[aretes[i][0][1],aretes[i][1][1]],linewidth=0.5,color='blue')
+    for i in range(len(Branch)):
+        plt.plot([Branch[i][0][0],Branch[i][1][0]],[Branch[i][0][1],Branch[i][1][1]],linewidth=0.5,color='red')
+    plt.axis('square')
+    plt.show()
+    plt.savefig('branchtest.png', dpi=500)
+
+
+## 3D TREE WITH COLORED BRANCH
+
+def branchplot3d(n):
+    A=idla3(n)
+    ax=plt.axes(projection="3d")
+    ax.set_box_aspect([1,1,1])
+    point=A[0][-1]
+    aretes=A[1]
+    Branch=branch(aretes,point)
+    for i in range(len(aretes)):
+        plt.plot([aretes[i][0][0],aretes[i][1][0]],[aretes[i][0][1],aretes[i][1][1]],[aretes[i][0][2],aretes[i][1][2]],linewidth=2,color='blue')
+    for i in range(len(Branch)):
+        plt.plot([Branch[i][0][0],Branch[i][1][0]],[Branch[i][0][1],Branch[i][1][1]],[Branch[i][0][2],Branch[i][1][2]],linewidth=2,color='red')
+    plt.axis('square')
+    plt.show()
 
